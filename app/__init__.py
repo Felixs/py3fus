740cf4078e7e6d27867b590a5794ba577cb7e965
Felix Sponholz <fsponholz@infonline.de>
2021-02-07 15:44:36 +0100

    chore: linting

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,16 @@
-# This file initializes your application and brings together all of the various components.
+# This file initializes your application and brings together all of
+# the various components.
 import os
-from flask import Flask, render_template, g, request,redirect, url_for, session
-from app.db import get_all_url_info, get_url_info
+from flask import Flask, render_template, request, redirect, url_for, session
 from . import name_generator
 from . import db
 import validators
 
+
 def create_app(test_config=None):
     ''' factory fuction to create app with flask run '''
-    app = Flask(__name__, instance_relative_config=True, template_folder='./templates', static_folder='./static')
+    app = Flask(__name__, instance_relative_config=True,
+                template_folder='./templates', static_folder='./static')
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'py3fus.sqlite'),
@@ -42,7 +44,7 @@ def create_app(test_config=None):
         # TODO: make auth token dynamic at startup or save to db?
         if 'authtoken' not in session or session['authtoken'] != 'testtoken':
             return render_template('admin_auth.html')
-            
+
         url_list = db.get_all_url_info()
         return render_template('admin.html', url_list=url_list)
 
@@ -65,10 +67,10 @@ def create_app(test_config=None):
     def short_url(short_url):
         ''' redirect page '''
         if short_url is not None and len(short_url) > 0:
-            db_data = get_url_info(short_url)
+            db_data = db.get_url_info(short_url)
             if db_data is not None:
                 return render_template('redirect.html', short_url=short_url, long_url=db_data[1])
-        
+
         return render_template('unknown_url.html', short_url=short_url)
 
     @app.errorhandler(404)
@@ -80,7 +82,3 @@ def create_app(test_config=None):
     db.init_app(app)
 
     return app
-
-
-
-
